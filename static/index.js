84e93d97cf7bec189e862790c73792d7cbd5bf87
Paul Lancaster <paul@lancasterzone.com>
2020-06-20 11:23:41 +0100

    Minimal required functionality reached

--- static/index.js
+++ static/index.js
@@ -1,6 +1,68 @@
 
+// A dictionary of current statuses.
+var current_status = {};
+
+function create_status_obj(name, addr, avg_rtt, last_loss, elm) {
+    var so = new Object();
+    so.name = name;
+    so.addr = addr;
+    so.avg_rtt = avg_rtt;
+    so.loss = last_loss;
+    so.elm = elm;
+
+    return so;
+}
+
+function gen_status_str(status) {
+    return "Name: " + status.name + " Addr: " + status.addr + " Avg_RTT: " + status.avg_rtt + " Loss: " + status.loss;
+}
+
+function create_status_row(id, status) {
+    $("#main_status_table").append(
+        "<tr class=\"status_row\" id=\"" + id + "\"><td>waiting...</td></tr>"
+    );
+}
+
+function update_status_row(elm, status) {
+    var jelm = $(elm);
+    jelm.text(gen_status_str(status));
+
+    if (status.loss > 0){
+        jelm.removeClass("ok_status")
+        jelm.addClass("lossy_status")
+    } else {
+        jelm.removeClass("lossy_status")
+        jelm.addClass("ok_status")
+    }
+}
+
+function update_status_display(statuses) {
+    for (var key in statuses){
+        var id = statuses[key].name.replace(" " , "");
+
+        x = document.getElementById(id);
+
+        if (x == null) {
+            create_status_row(id, statuses[key]);
+            x = document.getElementById(id);
+        }
+
+        update_status_row(x, statuses[key]);
+    }
+}
+
 function handle_host_status(res) {
-    console.log(res);
+    for (var key in res) {
+
+        var so = create_status_obj(res[key].name,
+             res[key].addr, 
+             res[key].avg_rtt, 
+             res[key].last_loss);
+
+        current_status[so.name] = so;
+    }
+
+    update_status_display(current_status);
 }
 
 function get_host_status() {
